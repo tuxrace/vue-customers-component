50672cf56257a1fe7b62b36ca865a3f1c0192a91
methodcase <methodcase@gmail.com>
2016-11-01 22:38:29 +0800

    change

--- main.js
+++ main.js
@@ -6,35 +6,45 @@ Vue.component('cu-button',{
 });
 
 Vue.component('item',{
-    props:['name'],
-    data:function(){
-        return {}
-    },
-    methods:{
-
-    },
-    template:`<div>{{name}} <button>x</button></div>`
+    props:['name','email','phone','event','id'],
+    template:`<div>{{name}} {{email}} {{phone}} <button v-on:click=event(id)> x </button> </div>`
 });
 
 Vue.component('customers',{
+    mounted:function(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(r => r.json())
+        .then(r => {this.customers = r})
+    },
     data:function(){
         return {
-            customers: ['x','g','r','a','e']
+            customers: this.customers
         }
     },
     methods:{
-        sort:function(){
-            this.customers.sort();
+        sort:function(){                        
+            this.customers.sort(function(a, b) {
+                var na = a.name.toUpperCase(); 
+                var nb = b.name.toUpperCase(); 
+                if (na < nb) 
+                    return -1;                
+                if (na > nb) 
+                    return 1;                
+                return 0;
+            });        
         },
         first: function(){
-            this.customers.splice(0,1);
+            this.customers.splice(1,this.customers.length);
+        },
+        remove: function(idx){            
+            this.customers.splice(idx,1)
         }
     },
     template: `<div>
                   <cu-button cap="Sort" v-bind:event=sort></cu-button>
                   <cu-button cap="First" v-bind:event=first></cu-button>
-                  <ul v-for="c in customers">
-                    <item :name=c></item>
+                  <ul v-for="(c, idx) in customers">
+                    <item :name=c.name :email=c.email :phone=c.phone :id=idx v-bind:event=remove></item>
                   </ul>
                </div>`
 });
